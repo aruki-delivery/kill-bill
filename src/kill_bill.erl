6d7fe56884ad0f9816193c7d07ba8c4b3710a48b
Carlos Brito Lage <carlos@carloslage.net>
2018-04-08 04:47:21 +0100

    oops

--- src/kill_bill.erl
+++ src/kill_bill.erl
@@ -321,13 +321,13 @@ create_session(WebAppName, WebAppConfig) ->
 					{purge_interval, 60},
 					{cluster_nodes, all},
 					{sync_mode, full}],
-			'Gibreel':create_cache(SessionCache, Options),
+			'Elixir.Gibreel':create_cache(SessionCache, Options),
 			SessionCache
 	end.
 
 stop_session(none) -> ok;
 stop_session(SessionCache) ->
-	'Gibreel':delete_cache(SessionCache).
+	'Elixir.Gibreel':delete_cache(SessionCache).
 
 get_server_paths(ServerName, Host, Server, Webapps) ->
 	case Server#server.webapps of 
